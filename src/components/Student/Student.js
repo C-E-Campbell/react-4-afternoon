a83a558d07ad6c1c50a74972f0a3a2b05c4c2bbc
charles <codelyfewithcharlie@gmail.com>
2019-10-15 15:07:18 -0700

    finished, weird css problem

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import axios from "axios";
 export default class Student extends Component {
-  constructor() {
-    super()
-
-  }
-
-  render() {
-    return (
-      <div className="box">
-        <h1>Student</h1>
-      </div>
-    )
-  }
+	constructor() {
+		super();
+		this.state = {
+			studentInfo: {}
+		};
+	}
+	componentDidMount() {
+		axios
+			.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+			.then(response => this.setState({ studentInfo: response.data }));
+	}
+	render() {
+		const { first_name, last_name, grade, email } = this.state.studentInfo;
+		return (
+			<div className='box'>
+				<h1>Student</h1>
+				<h3>{first_name}</h3>
+				<h3>{last_name}</h3>
+				<h3>{grade}</h3>
+				<h3>{email}</h3>
+			</div>
+		);
+	}
 }
